187442796e2e54499d0b3a6fcc33ed1fa81a8101
alvaroy96 <alv.correo@gmail.com>
2021-10-25 16:16:23 +0200

    Some TODOs

---
+++ pyNeuroZip/compressFunctions.py
@@ -0,0 +1,166 @@
+import copy
+import os
+import sys
+import time
+
+import lz4.frame
+import zstandard
+from pyNAVIS import *
+
+
+def loadCompressedAedat(directory, filePath, settings, verbose=True):
+    # --- Check the filePath ---
+    if not os.path.exists(directory + filePath) and verbose:
+        print("Unable to find the specified compressed aedat file. Aborting...")
+        sys.exit(1)
+
+    # --- Get dataset folder and file name ---
+    splitFilePath = filePath.split('/')
+    dataset = splitFilePath[0]
+    fileName = splitFilePath[1]
+
+    # --- Load data from compressed aedat file ---
+    timeIni = time.time()
+    if verbose:
+        print("Loading " + fileName + " (compressed aedat file)")
+
+    file = open(directory + filePath, "rb")
+    filedata = file.read()  # Read all the file
+    compressor = filedata[0:4].decode("utf-8").strip()
+    addressSize = filedata[4] + 1
+    compressedData = filedata[5:]
+    file.close()
+
+    timeEnd = time.time()
+    if verbose:
+        print("Compressed file loaded in " + '{0:.3f}'.format(timeEnd - timeIni) + " seconds")
+        print("Decompressing " + fileName + " with " + str(addressSize) + "-byte addresses through " +
+              compressor + " decompressor")
+    timeIni = time.time()
+
+    # --- Decompress data ---
+    if compressor == "ZSTD":
+        dctx = zstandard.ZstdDecompressor()
+        decompressedData = dctx.decompress(compressedData)
+    elif compressor == "LZ4":
+        decompressedData = lz4.frame.decompress(compressedData)
+    else:
+        print("Unable to perform " + compressor + " decompression. Aborting...")
+        sys.exit(1)
+
+    bytesPerSpike = addressSize + 4
+    numSpikes = len(decompressedData) / bytesPerSpike
+    if not numSpikes.is_integer():
+        print("Spikes are not a whole number. Something went wrong. Aborting...")
+        sys.exit(1)
+    else:
+        numSpikes = int(numSpikes)
+
+    addresses = []
+    timestamps = []
+
+    for i in range(numSpikes):
+        index = i * bytesPerSpike
+        addresses.append(decompressedData[index:(index + addressSize)])
+        timestamps.append(decompressedData[(index + addressSize):(index + bytesPerSpike)])
+
+    # Return the new spikes file
+    addresses = [int.from_bytes(x, "big") for x in addresses]  # Bytes to ints
+    timestamps = [int.from_bytes(x, "big") for x in timestamps]  # Bytes to ints
+    spikes_file = SpikesFile(addresses, timestamps)
+
+    # Return the modified settings
+    newSettings = copy.deepcopy(settings)
+    newSettings.address_size = addressSize
+
+    timeEnd = time.time()
+    if verbose:
+        print("Decompression achieved in " + '{0:.3f}'.format(timeEnd - timeIni) + " seconds")
+
+    return [spikes_file, newSettings]
+
+
+def compressAedat(eventsDir, filePath, settings, ignoreOverwrite=True, verbose=True, compressor="ZSTD"):
+    # --- Get bytes needed to address and timestamp representation ---
+    addressSize = int(round(settings.num_channels * (settings.mono_stereo + 1) *
+                            (settings.on_off_both + 1) / 256))
+    orgAddressSize = settings.address_size
+    # TODO: Timestamp size = 2
+    # orgTimestampSize = settings.
+
+    # --- Get dataset folder and file name ---
+    splitFilePath = filePath.split('/')
+    dataset = splitFilePath[0]
+    fileName = splitFilePath[1]
+
+    # --- Check the file and destination folder ---
+    compressedEventsDir = eventsDir + "../compressedEvents/"
+
+    if not ignoreOverwrite:
+        if os.path.exists(compressedEventsDir + filePath):
+            print("The compressed aedat file for this aedat file already exists\n"
+                  "Do you want to overwrite it? Y/N")
+            option = input()
+
+            if option == "N":
+                print("File compression has been cancelled")
+                return
+
+    if not os.path.exists(compressedEventsDir):
+        os.makedirs(compressedEventsDir)
+
+    if not os.path.exists(compressedEventsDir + dataset):
+        os.makedirs(compressedEventsDir + dataset)
+
+    # --- Load data from original aedat file ---
+    timeIni = time.time()
+    if verbose:
+        print("Loading " + fileName + " (original aedat file)")
+
+    # TODO: Optimize loadAEDAT
+    data = Loaders.loadAEDAT(eventsDir + filePath, settings)
+    addresses = data.addresses
+    timestamps = data.timestamps
+
+    timeEnd = time.time()
+    if verbose:
+        print("Original file loaded in " + '{0:.3f}'.format(timeEnd - timeIni) + " seconds")
+        print("Compressing " + fileName + " with " + str(orgAddressSize) + "-byte addresses into an aedat file with "
+              + str(addressSize) + "-byte addresses through " + compressor + " compressor")
+    timeIni = time.time()
+
+    # --- New data ---
+    header = bytearray()
+    newData = bytearray()
+
+    # Header definition
+    if compressor == "ZSTD":
+        header.extend(bytes("ZSTD", "utf-8"))  # 4 bytes for the compressor
+    elif compressor == "LZ4":
+        header.extend(bytes("LZ4 ", "utf-8"))  # 4 bytes for the compressor
+    else:
+        print("Compressor not recognized. Aborting...")
+        sys.exit(1)
+
+    header.extend((addressSize - 1).to_bytes(1, "big"))  # 1 byte for address size
+
+    # Reorder the data by discarding useless addresses bytes
+    for i in range(len(addresses) - 1):
+        newData.extend(addresses[i].to_bytes(addressSize, "big"))
+        newData.extend(timestamps[i].to_bytes(4, "big"))
+
+    # --- Compress and store the data ---
+    if compressor == "ZSTD":
+        cctx = zstandard.ZstdCompressor()
+        compressedData = cctx.compress(newData)
+    else:
+        compressedData = lz4.frame.compress(newData)
+
+    file = open(compressedEventsDir + filePath, "wb")
+    file.write(header)
+    file.write(compressedData)
+    file.close()
+
+    timeEnd = time.time()
+    if verbose:
+        print("Compression achieved in " + '{0:.3f}'.format(timeEnd - timeIni) + " seconds")
---
+++ pyNeuroZip/main.py
@@ -0,0 +1,91 @@
+import gc
+import time
+from tkinter.filedialog import askopenfilename
+
+from pyNAVIS import *
+import matplotlib.pyplot as plt
+from tkinter import Tk
+
+from compressFunctions import compressAedat, loadCompressedAedat
+
+if __name__ == '__main__':
+    # Define source settings
+    jAER_settings = MainSettings(num_channels=64, mono_stereo=1, on_off_both=1, address_size=4, ts_tick=1,
+                                 bin_size=10000)
+    MatLab_settings = MainSettings(num_channels=64, mono_stereo=1, on_off_both=1, address_size=2, ts_tick=0.2,
+                                   bin_size=10000)
+
+    settings = jAER_settings
+
+    # Find the original aedat file
+    print("Select a file in events folder")
+    Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+    splitPath = askopenfilename().split("/")
+
+    lenSplitPath = len(splitPath)
+    directory = "/".join(splitPath[0:lenSplitPath-2]) + "/"
+    filePath = "/".join(splitPath[lenSplitPath-2:lenSplitPath])
+
+    # --- COMPRESSED DATA ---
+    # Compress the original aedat file
+    compressAedat(directory, filePath, settings, False)
+    gc.collect()  # Cleaning memory
+
+    # Load the compressed aedat file
+    [compressedSpikes_File, newSettings] = loadCompressedAedat(directory + "../compressedEvents/", filePath, settings)
+    gc.collect()  # Cleaning memory
+
+    # Adapt the compressed aedat file
+    Functions.check_SpikesFile(compressedSpikes_File, newSettings)
+    compressedSpikes_File.timestamps = Functions.adapt_timestamps(compressedSpikes_File.timestamps, newSettings)
+
+    # Plots
+    print("Showing compressed file plots...")
+    timeIni = time.time()
+
+    Plots.spikegram(compressedSpikes_File, newSettings, verbose=True)
+    gc.collect()  # Cleaning memory
+    Plots.sonogram(compressedSpikes_File, newSettings, verbose=True)
+    gc.collect()  # Cleaning memory
+    Plots.histogram(compressedSpikes_File, newSettings, verbose=True)
+    gc.collect()  # Cleaning memory
+    Plots.average_activity(compressedSpikes_File, newSettings, verbose=True)
+    gc.collect()  # Cleaning memory
+    Plots.difference_between_LR(compressedSpikes_File, newSettings, verbose=True)
+
+    timeEnd = time.time()
+    print("Plots generation took " + '{0:.3f}'.format(timeEnd - timeIni) + " seconds")
+
+    plt.show()
+    gc.collect()  # Cleaning memory
+
+    # --- ORIGINAL DATA ---
+    # Load the original aedat file. Prints added to show loading time
+    timeIni = time.time()
+    spikes_info = Loaders.loadAEDAT(directory + filePath, settings)
+    timeEnd = time.time()
+    print("Load original aedat file has took: " + str(timeEnd - timeIni))
+    gc.collect()  # Cleaning memory
+
+    # Adapt the original aedat file
+    Functions.check_SpikesFile(spikes_info, settings)
+    spikes_info.timestamps = Functions.adapt_timestamps(spikes_info.timestamps, settings)
+
+    # Plots
+    print("Showing original file plots...")
+    timeIni = time.time()
+
+    Plots.spikegram(spikes_info, settings, verbose=True)
+    gc.collect()  # Cleaning memory
+    Plots.sonogram(spikes_info, settings, verbose=True)
+    gc.collect()  # Cleaning memory
+    Plots.histogram(spikes_info, settings, verbose=True)
+    gc.collect()  # Cleaning memory
+    Plots.average_activity(spikes_info, settings, verbose=True)
+    gc.collect()  # Cleaning memory
+    Plots.difference_between_LR(spikes_info, settings, verbose=True)
+
+    timeEnd = time.time()
+    print("Plots generation took " + '{0:.3f}'.format(timeEnd - timeIni) + " seconds")
+
+    plt.show()
