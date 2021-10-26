4a46f5ddaa303afb64fb939b5f8b547d984b1aa0
alvaroy96 <alv.correo@gmail.com>
2021-10-26 11:24:14 +0200

    Update CompressedHeader.py
    Added library version at the beginning of the compressed file header

--- AERzip/CompressedHeader.py
+++ AERzip/CompressedHeader.py
@@ -1,20 +1,20 @@
-import sys
-
-
 class CompressedHeader:
     def __init__(self, compressor="ZSTD", address_size=4, timestamp_size=4):
+        self.library_version = "AERzip v0.2.0"
         self.compressor = compressor
         self.address_size = address_size
         self.timestamp_size = timestamp_size
-        self.header_size = 6
+        self.header_size = len(self.library_version) + 5 + 1 + 1
 
     def toBytes(self):
         header = bytearray()
 
+        header.extend(self.library_version)
+
         if self.compressor == "ZSTD":
-            header.extend(bytes("ZSTD", "utf-8"))  # 4 bytes for the compressor
+            header.extend(bytes("ZSTD ", "utf-8"))  # 5 bytes for the compressor
         elif self.compressor == "LZ4":
-            header.extend(bytes("LZ4 ", "utf-8"))  # 4 bytes for the compressor
+            header.extend(bytes("LZ4  ", "utf-8"))  # 5 bytes for the compressor
         else:
             raise ValueError("Compressor not recognized. Aborting...")
 
