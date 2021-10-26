1b3a0d762086360d611d3f3077b34cde31d839b4
alvaroy96 <alv.correo@gmail.com>
2021-10-26 10:41:56 +0200

    Large modifications on compressAedat function
    compressAedat function has been renamed to compressDataFromFile. Paths used at this function have been fixed and minimum functions have been extracted from the function code (compressData, getCompressedFileHeader and discardBytes functions)

--- AERzip/compressFunctions.py
+++ AERzip/compressFunctions.py
@@ -7,7 +7,82 @@ import lz4.frame
 import zstandard
 from pyNAVIS import *
 
-# TODO: Fix paths
+
+def compressData(data, compressor="ZSTD"):
+    if compressor == "ZSTD":
+        cctx = zstandard.ZstdCompressor()
+        compressed_data = cctx.compress(data)
+    else:
+        compressed_data = lz4.frame.compress(data)
+
+    return compressed_data
+
+
+def compressDataFromFile(src_events_dir, dst_compressed_events_dir, dataset_name, file_name,
+                         settings, ignore_overwriting=True, verbose=True, compressor="ZSTD",
+                         store=True):
+    # --- Get bytes needed to address and timestamp representation ---
+    address_size = int(round(settings.num_channels * (settings.mono_stereo + 1) *
+                             (settings.on_off_both + 1) / 256))
+    org_address_size = settings.address_size
+    # TODO: Timestamp size = 2
+    # orgTimestampSize = settings.
+
+    # --- Check the file ---
+    if store and not ignore_overwriting:
+        if os.path.exists(dst_compressed_events_dir + dataset_name + file_name):
+            print("The compressed aedat file associated with this aedat file already exists\n"
+                  "Do you want to overwrite it? Y/N")
+            option = input()
+
+            if option == "N":
+                print("File compression has been cancelled")
+                return
+
+    # --- Load data from original aedat file ---
+    start_time = time.time()
+    if verbose:
+        print("Loading " + file_name + " (original aedat file)")
+
+    # TODO: Optimize loadAEDAT
+    spikes_file = Loaders.loadAEDAT(src_events_dir + dataset_name + file_name, settings)
+
+    end_time = time.time()
+    if verbose:
+        print("Original file loaded in " + '{0:.3f}'.format(end_time - start_time) + " seconds")
+        print("Compressing " + file_name + " with " + str(org_address_size) + "-byte addresses into an aedat file with "
+              + str(address_size) + "-byte addresses through " + compressor + " compressor")
+    start_time = time.time()
+
+    # --- New data ---
+    header = getCompressedFileHeader(compressor, address_size)
+    raw_smallest_data = discardBytes(spikes_file, address_size, 4)  # Discard useless bytes
+
+    # Compress the data
+    compressed_data = compressData(raw_smallest_data, compressor)
+
+    # Join header with compressed data
+    file_data = header.join(compressed_data)
+
+    # --- Store the data ---
+    if store:
+        # Check the destination folder
+        if not os.path.exists(dst_compressed_events_dir):
+            os.makedirs(dst_compressed_events_dir)
+
+        if not os.path.exists(dst_compressed_events_dir + dataset_name):
+            os.makedirs(dst_compressed_events_dir + dataset_name)
+
+        # Write the file
+        file = open(dst_compressed_events_dir + dataset_name + file_name, "wb")
+        file.write(file_data)
+        file.close()
+
+    end_time = time.time()
+    if verbose:
+        print("Compression achieved in " + '{0:.3f}'.format(end_time - start_time) + " seconds")
+
+    return file_data
 
 
 def loadCompressedAedat(directory, file_path, settings, verbose=True):
@@ -82,55 +157,9 @@ def loadCompressedAedat(directory, file_path, settings, verbose=True):
     return [spikes_file, new_settings]
 
 
-def compressAedat(events_dir, file_path, settings, ignore_overwriting=True, verbose=True, compressor="ZSTD",
-                  return_data=True):
-    # --- Get bytes needed to address and timestamp representation ---
-    address_size = int(round(settings.num_channels * (settings.mono_stereo + 1) *
-                            (settings.on_off_both + 1) / 256))
-    org_address_size = settings.address_size
-    # TODO: Timestamp size = 2
-    # orgTimestampSize = settings.
-
-    # --- Get dataset folder and file name ---
-    split_file_path = file_path.split('/')
-    dataset = split_file_path[0]
-    file_name = split_file_path[1]
-
-    # --- Check the file ---
-    compressed_events_dir = events_dir + "../compressedEvents/"
-
-    if not ignore_overwriting:
-        if os.path.exists(compressed_events_dir + file_path):
-            print("The compressed aedat file for this aedat file already exists\n"
-                  "Do you want to overwrite it? Y/N")
-            option = input()
-
-            if option == "N":
-                print("File compression has been cancelled")
-                return
-
-    # --- Load data from original aedat file ---
-    start_time = time.time()
-    if verbose:
-        print("Loading " + file_name + " (original aedat file)")
-
-    # TODO: Optimize loadAEDAT
-    data = Loaders.loadAEDAT(events_dir + file_path, settings)
-    addresses = data.addresses
-    timestamps = data.timestamps
-
-    end_time = time.time()
-    if verbose:
-        print("Original file loaded in " + '{0:.3f}'.format(end_time - start_time) + " seconds")
-        print("Compressing " + file_name + " with " + str(org_address_size) + "-byte addresses into an aedat file with "
-              + str(address_size) + "-byte addresses through " + compressor + " compressor")
-    start_time = time.time()
-
-    # --- New data ---
+def getCompressedFileHeader(compressor="ZSTD", address_size=4):
     header = bytearray()
-    raw_data = bytearray()
 
-    # Header definition
     if compressor == "ZSTD":
         header.extend(bytes("ZSTD", "utf-8"))  # 4 bytes for the compressor
     elif compressor == "LZ4":
@@ -141,35 +170,16 @@ def compressAedat(events_dir, file_path, settings, ignore_overwriting=True, verb
 
     header.extend((address_size - 1).to_bytes(1, "big"))  # 1 byte for address size
 
-    # Reorder the data by discarding useless addresses bytes
+    return header
+
+
+def discardBytes(spikes_file, address_size, timestamp_size):
+    raw_smallest_data = bytearray()
+    addresses = spikes_file.addresses
+    timestamps = spikes_file.timestamps
+
     for i in range(len(addresses) - 1):
-        raw_data.extend(addresses[i].to_bytes(address_size, "big"))
-        raw_data.extend(timestamps[i].to_bytes(4, "big"))
+        raw_smallest_data.extend(addresses[i].to_bytes(address_size, "big"))
+        raw_smallest_data.extend(timestamps[i].to_bytes(timestamp_size, "big"))
 
-    # --- Compress and store the data ---
-    if compressor == "ZSTD":
-        cctx = zstandard.ZstdCompressor()
-        compressed_data = cctx.compress(raw_data)
-    else:
-        compressed_data = lz4.frame.compress(raw_data)
-
-    # Join header with compressed data
-    file_data = header.join(compressed_data)
-
-    # Check the destination folder
-    if not os.path.exists(compressed_events_dir):
-        os.makedirs(compressed_events_dir)
-
-    if not os.path.exists(compressed_events_dir + dataset):
-        os.makedirs(compressed_events_dir + dataset)
-
-    file = open(compressed_events_dir + file_path, "wb")
-    file.write(file_data)
-    file.close()
-
-    end_time = time.time()
-    if verbose:
-        print("Compression achieved in " + '{0:.3f}'.format(end_time - start_time) + " seconds")
-
-    if return_data:
-        return file_data
+    return raw_smallest_data
