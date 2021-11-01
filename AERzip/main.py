689961788ab7fcfbb8642d1e1e12233131bee737
alvaroy96 <alv.correo@gmail.com>
2021-11-02 00:58:27 +0100

    Back again

--- AERzip/main.py
+++ AERzip/main.py
@@ -7,7 +7,7 @@ from tkinter.filedialog import askopenfilename
 import matplotlib.pyplot as plt
 from pyNAVIS import *
 
-from CompressionFunctions import decompressDataFromFile, compressDataFromFile
+from compressionFunctions import decompressDataFromFile, compressDataFromFile
 
 if __name__ == '__main__':
     root = Tk()
