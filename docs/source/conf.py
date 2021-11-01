07c12134b67a7f5407ad8773d20d6cfd225703b2
alvaroy96 <alv.correo@gmail.com>
2021-11-01 20:43:16 +0100

    Trying again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,7 @@ release = 'v0.6.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'numpydoc'
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
