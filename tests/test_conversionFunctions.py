6b3fb3c01c74d084957e9150cf0d3c8a81f6e2ea
alvayus <alv.correo@gmail.com>
2022-09-15 08:02:04 +0200

    conversionFunctions test has been improved

--- tests/test_conversionFunctions.py
+++ tests/test_conversionFunctions.py
@@ -1,39 +1,52 @@
+import copy
 import unittest
 
 from pyNAVIS import MainSettings, Loaders
 
-from AERzip.CompressedFileHeader import CompressedFileHeader
 from AERzip.conversionFunctions import calcRequiredBytes, spikesFileToBytes, bytesToSpikesFile
 
 
 class JAERSettingsTest(unittest.TestCase):
 
     def setUp(self):
-        # Create settings object
-        self.file_settings = MainSettings(num_channels=64, mono_stereo=1, on_off_both=1, address_size=4,
-                                          timestamp_size=4, ts_tick=1, bin_size=10000)
+        # Defining settings
+        self.file_settings_stereo_4_4 = MainSettings(num_channels=64, mono_stereo=1, on_off_both=1, address_size=4,
+                                                     timestamp_size=4, ts_tick=1, bin_size=10000)
+        self.file_settings_mono_2_4 = MainSettings(num_channels=64, mono_stereo=0, on_off_both=1, address_size=2,
+                                                   timestamp_size=4, ts_tick=1, bin_size=10000)
 
-        # Loading a spikes_file
-        self.file_path = "events/dataset/enun_stereo_64ch_ONOFF_addr4b_ts1.aedat"
-        self.spikes_file = Loaders.loadAEDAT(self.file_path, self.file_settings)
-
-        # Getting target sizes
-        self.final_address_size, self.final_timestamp_size = calcBytesToPrune(self.spikes_file, self.file_settings)
+        # Loading spikes_files
+        self.files_data = [("events/dataset/enun_stereo_64ch_ONOFF_addr4b_ts1.aedat", self.file_settings_stereo_4_4),
+                           ("events/dataset/130Hz_mono_64ch_ONOFF_addr2b_ts02.aedat", self.file_settings_mono_2_4)]
+        self.spikes_files = []
+        for file_data in self.files_data:
+            self.spikes_files.append(Loaders.loadAEDAT(file_data[0], file_data[1]))
 
     def test_spikesFileToBytesAndViceversa(self):
-        # spikes_file to raw bytes
-        bytes_data = spikesFileToBytes(self.spikes_file, self.file_settings, self.final_address_size, self.final_timestamp_size)
+        for i in range(len(self.spikes_files)):
+            spikes_file = self.spikes_files[i]
+            file_settings = self.files_data[i][1]
 
-        # Raw bytes to spikes_file
-        new_settings = self.file_settings
-        new_settings.address_size = self.final_address_size
-        new_settings.timestamp_size = self.final_timestamp_size
+            # Getting target sizes
+            final_address_size, final_timestamp_size = calcRequiredBytes(spikes_file, file_settings)
 
-        spikes_file, _ = bytesToSpikesFile(bytes_data, new_settings)
+            # spikes_file to raw bytes
+            bytes_data = spikesFileToBytes(spikes_file, file_settings, final_address_size, final_timestamp_size)
 
-        # Compare original and final spikes_file
-        self.assertEqual(self.spikes_file.addresses.tolist(), spikes_file.addresses.tolist())
-        self.assertEqual(self.spikes_file.timestamps.tolist(), spikes_file.timestamps.tolist())
+            # Raw bytes to spikes_file
+            compressed_settings = copy.deepcopy(file_settings)
+            compressed_settings.address_size = final_address_size
+            compressed_settings.timestamp_size = final_timestamp_size
+
+            new_spikes_file, _ = bytesToSpikesFile(bytes_data, compressed_settings)
+
+            # Compare original and final spikes_file
+            self.assertEqual(spikes_file.addresses.tolist(), new_spikes_file.addresses.tolist())
+            self.assertEqual(spikes_file.timestamps.tolist(), new_spikes_file.timestamps.tolist())
+            self.assertEqual(spikes_file.max_ts, new_spikes_file.max_ts)
+            self.assertEqual(spikes_file.min_ts, new_spikes_file.min_ts)
+            self.assertEqual(spikes_file.max_ts_index, new_spikes_file.max_ts_index)
+            self.assertEqual(spikes_file.min_ts_index, new_spikes_file.min_ts_index)
 
 
 if __name__ == '__main__':
