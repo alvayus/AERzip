5f81f6a20572534104ccbbfad04806f7595b06e7
alvayus <alv.correo@gmail.com>
2022-09-15 18:38:48 +0200

    CompressedFileHeader now stores pyNAVIS information (Header has been formatted)

--- AERzip/CompressedFileHeader.py
+++ AERzip/CompressedFileHeader.py
@@ -8,61 +8,51 @@ class CompressedFileHeader:
     These are the main attributes of this class:
     - library_version (string): A string indicating the library version.
     - compressor (string): A string indicating the compressor used.
-    - num_channels (int): An int indicating the number of channels of the NAS.
-    - mono_stereo (int): An int indicating if the file is mono or stereo.
-    - ts_tick (float): A float indicating the correspondence factor for timestamps.
-    - on_off (int): An int indicating if the addresses correspond to ON or OFF (or both) spikes
     - address_size (int): An int indicating the size of the addresses contained in the compressed file.
     - timestamp_size (int): An int indicating the size of the timestamps contained in the compressed file.
-    - end_header (string): The string that represents the end of the header in generic aedat files.
-    - header_size (int): An int indicating the total length of the header in the compressed file.
+    - header_end (string): The string that represents the end of the header in generic aedat files.
 
     Each attribute has an int associated which represents its length in bytes inside the compressed aedat file. These
     ints are not stored in the file and they cannot be changed since they are fixed in the CompressedFileHeader's
     constructor method, in order to allow compatibility between different versions of AERzip.
     """
 
-    def __init__(self, compressor, settings):
-        """
-        Constructor of CompressedFileHeader objects.
-
-        :param string compressor: A string indicating the compressor to be used.
-        :param MainSettings settings: A MainSettings object from pyNAVIS containing the required fields for the building of the compressed file.
-        """
-        if not (self.compressor == "ZSTD" or self.compressor == "LZ4" or self.compressor == "LZMA"):
+    def __init__(self, compressor, address_size, timestamp_size):
+        # Checks before object creation
+        if not (compressor == "ZSTD" or compressor == "LZ4" or compressor == "LZMA"):
             raise ValueError("Only ZSTD, LZ4 or LZMA compression algorithms are supported for now")
 
-        if not settings or not isinstance(settings, MainSettings):
-            raise ValueError("Settings must be specified as a MainSettings object from pyNAVIS")
+        # Sizes of fixed fields
+        self.__library_version_length = 20
+        self.__compressor_length = 10
+        self.__address_size_length = 4  # 32-bit int
+        self.__timestamp_size_length = 4  # 32-bit int
+        self.__optional_length = 40
+        self.__optional_available = 40  # Allows to control the space available in the optional field
+        self.__header_end_length = 22  # Size of fixed string "#End Of ASCII Header\r\n"
+        self.__header_length = self.__library_version_length + self.__compressor_length + self.__address_size_length + self.__timestamp_size_length + self.__optional_length + self.__header_end_length
 
+        # Values for fixed fields
         self.library_version = "AERzip v0.6.5"
-        self.library_version_length = 20
-
         self.compressor = compressor
-        self.compressor_length = 10
+        self.address_size = address_size
+        self.timestamp_size = timestamp_size
+        self.optional = bytearray()
+        self.header_end = "#End Of ASCII Header\r\n"
 
-        self.num_channels = settings.num_channels
-        self.num_channels_length = 4  # 32-bit int
+    def addOptional(self, data_bytes):
+        """
+        Adds bytes from data_bytes to the optional field of the header.
 
-        self.mono_stereo = settings.mono_stereo
-        self.mono_stereo_length = 1  # 8-bit int. Minimum data size in Python
+        :param bytearray data_bytes: Bytes to add to the optional field
+        """
+        data_bytes_len = len(data_bytes)
 
-        self.ts_tick = settings.ts_tick
-        self.ts_tick_length = 4  # 32-bit float
+        if data_bytes_len > self.__optional_available:
+            raise MemoryError("The optional field has reached its maximum capacity. No more information can be added")
 
-        self.on_off_both = settings.on_off_both
-        self.on_off_both_length = 1  # 8-bit int. Minimum data size in Python
-
-        self.address_size = settings.address_size
-        self.address_length = 4  # 32-bit int
-
-        self.timestamp_size = settings.timestamp_size
-        self.timestamp_length = 4  # 32-bit int
-
-        self.end_header = "#End Of ASCII Header\r\n"
-        self.end_header_length = 22  # Fixed value (compatibility with other versions?)
-
-        self.header_size = self.library_version_length + self.compressor_length + self.num_channels_length + self.mono_stereo_length + self.ts_tick_length + self.on_off_both_length + self.address_length + self.timestamp_length + self.end_header_length
+        self.optional.extend(data_bytes)
+        self.__optional_available -= data_bytes_len
 
     def toBytes(self):
         """
@@ -74,19 +64,43 @@ class CompressedFileHeader:
         """
         header_bytes = bytearray()
 
-        # Library version and compressor used
-        header_bytes.extend(bytes(self.library_version.ljust(self.library_version_length), "utf-8"))
-        header_bytes.extend(bytes(self.compressor.ljust(self.compressor_length), "utf-8"))
+        # Fixed fields
+        header_bytes.extend(bytes(self.library_version.ljust(self.__library_version_length), "utf-8"))
+        header_bytes.extend(bytes(self.compressor.ljust(self.__compressor_length), "utf-8"))
+        header_bytes.extend(self.address_size.to_bytes(self.__address_size_length, "big"))
+        header_bytes.extend(self.timestamp_size.to_bytes(self.__timestamp_size_length, "big"))
 
         # MainSetting fields (with or without pruning)
-        header_bytes.extend(self.num_channels.to_bytes(self.num_channels_length, "big"))
-        header_bytes.extend(self.mono_stereo.to_bytes(self.mono_stereo_length, "big"))
-        header_bytes.extend(self.ts_tick.to_bytes(self.ts_tick_length, "big"))
-        header_bytes.extend(self.on_off_both.to_bytes(self.on_off_both_length, "big"))
-        header_bytes.extend(self.address_size.to_bytes(self.address_length, "big"))
-        header_bytes.extend(self.timestamp_size.to_bytes(self.timestamp_length, "big"))
+        header_bytes.extend(self.optional.ljust(self.__optional_length))
 
         # End of the header
-        header_bytes.extend(bytes(self.end_header.ljust(self.end_header_length), "utf-8"))
+        header_bytes.extend(bytes(self.header_end.ljust(self.__header_end_length), "utf-8"))
 
         return header_bytes
+
+    def addMainSettings(self, settings):
+        """
+        Stores extra information of a MainSettings object from pyNAVIS in the optional field of the header. This
+        information is summarized here:
+        - num_channels (int): An int indicating the number of channels of the NAS.
+        - mono_stereo (int): An int indicating if the file is mono or stereo.
+        - ts_tick (float): A float indicating the correspondence factor for timestamps.
+        - on_off (int): An int indicating if the addresses correspond to ON or OFF (or both) spikes
+
+        :param MainSettings settings: A MainSettings object from pyNAVIS.
+        """
+        # Checking settings
+        if not isinstance(settings, MainSettings):
+            raise ValueError("Settings must be specified as a MainSettings object from pyNAVIS")
+
+        # Fields to bytes
+        num_channels_bytes = settings.num_channels.to_bytes(4, "big")  # 32-bit int
+        mono_stereo_bytes = settings.mono_stereo.to_bytes(1, "big")  # 8-bit int. Minimum data size in Python
+        ts_tick_bytes = settings.ts_tick.to_bytes(4, "big")  # 32-bit float
+        on_off_both_bytes = settings.on_off_both.to_bytes(1, "big")  # 8-bit int. Minimum data size in Python
+
+        # Adding bytes to optional field
+        self.addOptional(num_channels_bytes)
+        self.addOptional(mono_stereo_bytes)
+        self.addOptional(ts_tick_bytes)
+        self.addOptional(on_off_both_bytes)
