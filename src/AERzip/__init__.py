dae5da594e3aff35b20606420d6689e083285bb5
Álvaro Ayuso <alv.correo@gmail.com>
2025-11-03 15:42:30 +0100

    AERzip version 1.0.0

--- src/AERzip/__init__.py
+++ src/AERzip/__init__.py
@@ -1,9 +1,6 @@
-__version__ = "0.8.0"
+__version__ = "1.0.0"
 
 from .CompressedFileHeader import CompressedFileHeader
-from .compressionFunctions import compressDataFromStoredNASFile, extractDataFromCompressedFile, bytesToCompressedFile, compressedFileToBytes, spikesFileToCompressedFile, compressedFileToSpikesFile, extractCompressedData, compressData, decompressData, getCompressedFile, storeFile, checkFileExists, loadFile
-from .conversionFunctions import bytesToSpikesFile, spikesFileToBytes, calcRequiredBytes, constructStruct
+from .functions import(saveCompressedFile, loadCompressedFile)
 
-__all__ = ["CompressedFileHeader", 
-           "compressDataFromStoredNASFile", "extractDataFromCompressedFile", "bytesToCompressedFile", "compressedFileToBytes", "spikesFileToCompressedFile", "compressedFileToSpikesFile", "extractCompressedData", "compressData", "decompressData", "getCompressedFile", "storeFile", "checkFileExists", "loadFile",
-           "bytesToSpikesFile", "spikesFileToBytes", "calcRequiredBytes", "constructStruct"]
+__all__ = ["CompressedFileHeader", "saveCompressedFile", "loadCompressedFile"]
